6bfa418f03c35b4ad01cf7ac9e13c4ce9c26f61b
Stijn <s.rotman@tutanota.com>
2025-02-21 14:59:47 +0100

    Add tests to prepare for explicit paa function.

--- tests/reference.py
+++ tests/reference.py
@@ -4,9 +4,6 @@ import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 from scipy.stats import norm, zscore
 
-# windows = (n - window) / stride + 1
-# paa in O(windows)
-
 
 def discretise(ts, window, stride, w, alpha):
     # Create sliding windows and z-normalise each sliding window
@@ -23,3 +20,16 @@ def discretise(ts, window, stride, w, alpha):
     sax = np.digitize(paa, breakpoints)
 
     return sax.tolist()
+
+
+def paa_window(ts, window, stride, w):
+    # Create sliding windows and z-normalise each sliding window
+    windows = sliding_window_view(ts, window)[::stride]
+    windows = zscore(windows, axis=1)
+
+    # PAA
+    indices = np.arange(window * w) // w
+    indices = indices.reshape(w, -1)
+    paa = windows[:, indices].mean(axis=2)
+
+    return paa.tolist()
---
+++ tests/test_paa.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+from reference import paa_window
+from sax_ts import paa
+
+np.random.seed(123)
+a = np.arange(20)
+b = np.random.random(1000)
+
+D = [a]
+WIN = [8, 12, 16, 20]
+S = [2, 3, 5, 8]
+W = [3, 4, 5, 6, 7, 8]
+A = range(4, 12)
+
+window_params = [(data, window, 1, 4) for data in D for window in WIN]
+stride_params = [(data, 16, stride, 5) for data in D for stride in S]
+w_divides_window_params = [(data, 16, 1, w) for data in D for w in W[1::2]]
+w_no_divides_window_params = [(data, 16, 1, w) for data in D for w in W[::2]]
+
+
+@pytest.mark.parametrize("test_input,window,stride,w", window_params)
+def test_window(test_input, window, stride, w):
+    expected = paa_window(test_input, window, stride, w)
+    assert_allclose(paa(test_input, window, stride, w), expected, atol=1e-7)
+
+
+@pytest.mark.parametrize("test_input,window,stride,w", stride_params)
+def test_stride(test_input, window, stride, w):
+    expected = paa_window(test_input, window, stride, w)
+    assert_allclose(paa(test_input, window, stride, w), expected, atol=1e-7)
+
+
+@pytest.mark.parametrize("test_input,window,stride,w", w_divides_window_params)
+def test_w_divides_window(test_input, window, stride, w):
+    expected = paa_window(test_input, window, stride, w)
+    assert_allclose(paa(test_input, window, stride, w), expected, atol=1e-7)
+
+
+@pytest.mark.parametrize("test_input,window,stride,w", w_no_divides_window_params)
+def test_w_no_divides_window(test_input, window, stride, w):
+    expected = paa_window(test_input, window, stride, w)
+    assert_allclose(paa(test_input, window, stride, w), expected, atol=1e-7)
