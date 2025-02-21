ca00969ae1b439b9a542d834b448369ad9e02629
Stijn <s.rotman@tutanota.com>
2025-02-21 14:27:29 +0100

    Remove old test.py (obselete because of tests/).

--- test.py
+++
@@ -1,8 +0,0 @@
-from sax_ts import sax
-
-from tests.reference import discretise
-
-data = [[-1.0, 2.0, 0.1, -1.0, 1.0, -1.0], [1.0, 3.2, -1.0, -3.0, 1.0, -1.0]]
-for row in data:
-    print(sax(row, 6, 1, 3, 2))
-    print(discretise(row, 6, 1, 3, 2))
