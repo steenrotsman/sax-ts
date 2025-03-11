42fb5d021bb92b59b866b0dbf9d975733c6aadb0
stijn <s.j.rotman@tilburguniversity.edu>
2025-03-11 16:33:11 +0100

    Add autoformat.

--- src/bind.cpp
+++ src/bind.cpp
@@ -1,10 +1,13 @@
+#include "sax.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "sax.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(sax_ts, m) {
-    m.def("sax", &sax, "Apply SAX to a single time series.", py::arg("ts"), py::arg("window"), py::arg("stride")=1, py::arg("w")=15, py::arg("alpha")=4);
-    m.def("paa", &paa, "Apply PAA to a single time series.", py::arg("ts"), py::arg("window"), py::arg("stride")=1, py::arg("w")=15);
+  m.def("sax", &sax, "Apply SAX to a single time series.", py::arg("ts"),
+        py::arg("window"), py::arg("stride") = 1, py::arg("w") = 15,
+        py::arg("alpha") = 4);
+  m.def("paa", &paa, "Apply PAA to a single time series.", py::arg("ts"),
+        py::arg("window"), py::arg("stride") = 1, py::arg("w") = 15);
 }
